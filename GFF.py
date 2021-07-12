4e14cee0c7d08285c10d738545a7a43942cbef43
donglf <here0009@gmail.com>
2021-07-12 10:19:40 +0800

    Add add_utrs function for the script

--- GFF.py
+++ GFF.py
@@ -3,9 +3,28 @@
 Classes and Parameters for GFF conversion
 """
 
-SELECTED_TYPES = {'mRNA':'mRNA', 'transcript':'transcript'}  # col3 : col8 ID_type
-SELECTED_SUB_TYPES = {'CDS':'CDS', 'intron':'intron', 'five_prime_UTR':'5_UTR', 'three_prime_UTR':'3_UTR'}
-SELECTED_ATTRIBUTES = {'mRNA':['ID','Name', 'Parent'], 'transcript':['ID','Name', 'Parent'], 'CDS':['Parent'], 'intron':['Parent'], 'five_prime_UTR':['Parent'], 'three_prime_UTR':['Parent'], 'exon':['Parent']}
+SELECTED_TYPES = {'mRNA', 'transcript'}  # col3 : col8 ID_type
+SELECTED_SUB_TYPES = {'CDS', 'exon', 'five_prime_UTR', 'three_prime_UTR', 'intron'}
+WRITE_SUB_TYPES = {'CDS',  '5_UTR', '3_UTR', 'intron'}
+SELECTED_ATTRIBUTES =  {'mRNA':['ID','Name', 'Parent'], 'transcript':['ID','Name', 'Parent'], 'CDS':['Parent'], 'intron':['Parent'], 'five_prime_UTR':['Parent'], 'three_prime_UTR':['Parent'], 'exon':['Parent']}
+WRITE_ATTRIBUTES = {'mRNA':['ID','Name'], 'transcript':['ID','Name'], 'CDS':['Parent'], 'intron':['Parent'], 'five_prime_UTR':['Parent'], 'three_prime_UTR':['Parent'], 'exon':['Parent']}
+TYPE_TRANS_DICT = {'CDS': 'CDS',  'five_prime_UTR': '5_UTR', 'three_prime_UTR': '3_UTR',
+                   'intron': 'intron', 'mRNA': 'mRNA', 'transcript': 'transcript', 'exon': 'exon'}
+ATRB_TRANS_DICT = {'ID': 'ID', 'Name': 'name', 'Parent': 'Parent'}
+
+
+class TransTables():
+    """
+    Store the dictionaries for name transformation
+    """
+    def __init__(self):
+        self.tables = dict()
+
+    def add_table(self, name, dictionary):
+        self.tables[name] = dictionary
+
+    def get_table(self, name):
+        return self.tables.get(name, None)
 
 class GffAttributes:
     """
@@ -16,71 +35,159 @@ class GffAttributes:
         self.gff_style = gff_style
         self.atrb_dict = {}
         if self.string != '':
-            self._lst = attributes.split(';')
-            for _atrb in self._lst:
-                if '=' not in _atrb:
+            self.lst = attributes.split(';')
+            for atrb in self.lst:
+                atrb = atrb.strip()  # remove extra spaces if any
+                if '=' not in atrb:
                     continue
-                _key, _val = _atrb.split('=')
-                self.atrb_dict[_key] = _val
+                key, val = atrb.split('=')
+                self.atrb_dict[key] = val
         if self.gff_style == 'ENSEMBL':
             self.atrb_id_sep = ':'  # e.g. ID=transcript:ENST00000423372;Parent=gene:ENSG00000237683
         elif self.gff_style == 'NCBI':
             self.atrb_id_sep = '-' # e.g. ID=exon-NM_001005277.1-1;Parent=rna-NM_001005277.1
     
     def __str__(self):
-        return ';'.join([f'{k}={v}'for k, v in self.atrb_dict.items()])
+        return self.string
     
-    def trimm_atrb(self, feature):
-        if feature not in SELECTED_ATTRIBUTES:
+    def __repr__(self):
+        return self.string
+    
+    def trimm_atrb(self, type_name, atrb_trans_dict):
+        # type_name is name of type or subtype
+        if type_name not in SELECTED_ATTRIBUTES:
             return GffAttributes('', self.gff_style)
         res = []
-        if self.gff_style == 'NCBI' and feature in SELECTED_TYPES: # use the gene name if got one
-            if  'Parent' != 'None':
+        if self.gff_style == 'NCBI' and type_name in SELECTED_TYPES: # NCBI use "NM_*"  or "NR_* "as name, replace it with the gene name if got one
+            if 'Parent' in self.atrb_dict:
                 self.atrb_dict['Name'] = self.atrb_dict['Parent']
                 del self.atrb_dict['Parent']
-        for k in SELECTED_ATTRIBUTES[feature]:
+        for k in WRITE_ATTRIBUTES[type_name]:
             if k in self.atrb_dict:
                 v = self.atrb_dict[k]
                 idx = v.find(self.atrb_id_sep)
                 if idx != -1:
                     v = v[idx + 1:]
-                res.append(f'{k}={v}')
-        return GffAttributes(';'.join(res), self.gff_style)
+                if k == 'ID' or k == 'Parent':  # remove the version after dot for id and parent
+                    v = v.split('.')[0]
+                res.append(f'{atrb_trans_dict[k]}={v}')  # transfer Name to name
+        return GffAttributes('; '.join(res), self.gff_style)
 
 
 class GffRecord:
     """
-    gff record got 8 cols:
+    gff record got 9 cols:
     seqid source type start end score strand phase attributes
     """
-    def __init__(self, records, gff_style):
-        self.string = records
-        self.records = records.strip().split('\t')
-        self.seqid = self.records[0]
+    def __init__(self, record, gff_style):
+        self.string = record
+        lst = record.strip().split('\t')
+        assert len(lst) == 9
+        self.seqid = lst[0]
         self.source = gff_style
-        self.type = self.records[2]
-        self.start = self.records[3]
-        self.end = self.records[4]
-        self.score = self.records[5]
-        self.strand = self.records[6]
-        self.phase = self.records[7] 
-        self.attributes = GffAttributes(self.records[8], gff_style)
+        self.type = lst[2]
+        self.start = lst[3]
+        self.end = lst[4]
+        self.score = lst[5]
+        self.strand = lst[6]
+        self.phase = lst[7] 
+        self.attributes = GffAttributes(lst[8], gff_style)
         self.id = self.attributes.atrb_dict.get('ID', None)
         self.parent = self.attributes.atrb_dict.get('Parent', None)
-        self.name = self.attributes.atrb_dict.get('Name', None)
+        self.name = self.attributes.atrb_dict.get(
+            'Name', None) or self.attributes.atrb_dict.get('name', None) # Attributes 'Name' will be converted to 'name'
+        self.children = dict()
         
     def __str__(self):
         return self.string
     
     def __repr__(self):
         return self.string
+    
+    def add_children(self, gff_record):
+        if gff_record.type not in self.children:
+            self.children[gff_record.type] = []
+        self.children[gff_record.type].append(gff_record)
+    
+    def get_children(self, sub_type):
+        return self.children.get(sub_type, [])
+    
+    def get_coordinates(self, sub_type):
+        return [(int(gff.start), int(gff.end)) for gff in self.get_children(sub_type)]
+    
+    def add_coordinates(self, sub_type, coordinates):
+        """
+        add coordinates as gff record to self.children[sub_type]
+        """
+        for start, end in coordinates:
+            gff = GffRecord('\t'.join([self.seqid, self.source, sub_type, str(start), str(end), '.', self.strand, '.', f'Parent={self.id}']), self.source)
+            self.add_children(gff)
         
-    def convert(self, seqid_trans_dict):
-        convert_seqid = seqid_trans_dict[self.seqid]
-        convert_type = self.type
-        convert_atrb = self.attributes.trimm_atrb(self.type).string
-        if self.type in SELECTED_TYPES:
-            convert_type = SELECTED_TYPES[self.type]
-        elif self.type in SELECTED_SUB_TYPES:
-            convert_type = SELECTED_SUB_TYPES[self.type]
-        return  GffRecord('\t'.join([convert_seqid, self.source, convert_type, self.start, self.end, self.score, self.strand, self.phase, convert_atrb]), self.source)
+    def convert(self, tables):
+        """
+        convert to seqid, type,  and trimm atrb according to tables
+        """
+        if 'seqid' in tables and self.seqid in tables['seqid']:
+            seqid = tables['seqid'][self.seqid]
+        else:
+            seqid = self.seqid
+        if 'type' in tables and self.type in tables['type']:
+            convert_type = tables['type'][self.type]
+        else:
+            convert_type = self.type
+        if 'atrb' in tables:
+            atrb = self.attributes.trimm_atrb(self.type, tables['atrb']).string
+        else:
+            atrb = self.atrb.string
+        return  GffRecord('\t'.join([seqid, self.source, convert_type, self.start, self.end, self.score, self.strand, self.phase, atrb]), self.source)
+
+
+class RNA(GffRecord):
+    """
+    For mRNA, transcripts, we can add introns or utrs to it
+    """ 
+    def get_introns(self):
+        introns = []
+        exons = sorted(self.get_coordinates('exon'))
+        for i in range(len(exons) - 1):
+            if exons[i + 1][0] - 1 >= exons[i][1] + 1:
+                introns.append((exons[i][1] + 1, exons[i + 1][0] - 1))
+        return introns
+                
+    def get_utrs(self):
+        left_utrs = []
+        right_utrs = []
+        cds = sorted(self.get_coordinates('CDS'))
+        exons = sorted(self.get_coordinates('exon'))
+        assert self.strand in '-+'
+        if cds and exons:
+            cds_start = cds[0][0]
+            for s, e in exons:
+                if e < cds_start:
+                    left_utrs.append((s, e))
+                elif s < cds_start:
+                    left_utrs.append((s, cds_start - 1))
+                else:
+                    break
+            cds_end = cds[-1][-1]
+            for s, e in exons[::-1]:
+                if s > cds_end:
+                    right_utrs.append((s, e))
+                elif e > cds_end:
+                    right_utrs.append((cds_end + 1, e))
+                else:
+                    break
+        # return five_prime_utrs and three_prime_utrs
+        if self.strand == '+':
+            return left_utrs, right_utrs
+        else: 
+            return right_utrs, left_utrs
+    
+    def add_intron(self):
+        intron_coords = self.get_introns()
+        self.add_coordinates('intron', intron_coords)
+
+    def add_utrs(self):
+        five_prime_utrs, three_prime_utrs = self.get_utrs()
+        self.add_coordinates('5_UTR', five_prime_utrs)
+        self.add_coordinates('3_UTR', three_prime_utrs)
--- gff_converter.py
+++ gff_converter.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env python3
 """
-Input:
-Output:
-Usage:
-    gff_converter.py [-h] -i File -o File [-a File] -s str [--add_intron]
+usage: gff_converter.py [-h] -i input_gff -o output_gff [-a assembly_report] -t output_name_id_table -s gff_style [--add_intron] [--add_utr]
+
+Convert NCBI/ENSEMBL style Gff file to a concise UCSC style
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i input_gff, --input input_gff
+                        Input Gff File (default: None)
+  -o output_gff, --output output_gff
+                        Output Gff File (default: None)
+  -a assembly_report, --assembly assembly_report
+                        Assembly Report File (default: data/GCF_000001405.25_GRCh37.p13_assembly_report.txt)
+  -t output_name_id_table, --table output_name_id_table
+                        Output Name Id table File (default: None)
+  -s gff_style, --style gff_style
+                        Gff style, "ENSEMBL" or "NCBI" (default: None)
+  --add_intron          Add intron to gff file (default: False)
+  --add_utr             Add UTR to gff file (default: False)
+
 Example:
-    python3 gff_converter.py -i ensembl_test.gff3 -o ensembl_test_out2.gff -a data/GCF_000001405.25_GRCh37.p13_assembly_report.txt -s ENSEMBL --add_intron
-    python3 gff_converter.py -i ensembl_test.gff3 -o ensembl_test_out2.gff -s ENSEMBL --add_intron
-    python3 gff_converter.py ncbi_test.gff ncbi_test_out.gff data/GCF_000001405.25_GRCh37.p13_assembly_report.txt NCBI
+
+Test Data:
+    python3 gff_converter.py -i test/ncbi_test.gff -o test/ncbi_test_output.gff -a data/GCF_000001405.25_GRCh37.p13_assembly_report.txt -t test/ncbi_name_id.tsv -s NCBI  --add_intron --add_utr
+
+    python3 gff_converter.py -i test/ensembl_test.gff3 -o test/ensembl_test_output.gff -a data/GCF_000001405.25_GRCh37.p13_assembly_report.txt -t test/ensembl_name_id.tsv -s ENSEMBL  --add_intron
+
+Real Data:
+    python3 gff_converter.py -i gff_data/GCF_000001405.25_GRCh37.p13_genomic.gff -o gff_out/NCBI_output.gff -a data/GCF_000001405.25_GRCh37.p13_assembly_report.txt -t  gff_out/ncbi_name_id.tsv -s NCBI --add_intron --add_utr
+
+    python3 gff_converter.py -i gff_data/Homo_sapiens.GRCh37.87.Ensembl.gff3  -o gff_out/ENSEMBL_output.gff -a data/GCF_000001405.25_GRCh37.p13_assembly_report.txt -t  gff_out/ensembl_name_id.tsv -s ENSEMBL --add_intron
 """
 
 
 import argparse
 from typing import NamedTuple, TextIO
-from GFF import GffRecord, GffAttributes, SELECTED_TYPES, SELECTED_SUB_TYPES, SELECTED_ATTRIBUTES
-
+from collections import OrderedDict
+from GFF import RNA, GffRecord, TransTables, SELECTED_TYPES, SELECTED_SUB_TYPES, WRITE_SUB_TYPES, TYPE_TRANS_DICT, ATRB_TRANS_DICT
 
 class Arags(NamedTuple):
     """
@@ -23,96 +45,185 @@ class Arags(NamedTuple):
     gff_file:TextIO
     out_file:TextIO
     assembly_report_file:TextIO
+    name_id_table:TextIO
     gff_style:str
     add_intron:bool
+    add_utr:bool
 
 
 def get_args():
     """ 
     Get command-line arguments
     """
-    parser = argparse.ArgumentParser(description="Convert NCBI/ENSEMBL style GFF file to a concise UCSC style", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-i', '--input', metavar='File', type=argparse.FileType('r'), help='Input GFF File', required=True)
-    parser.add_argument('-o', '--output', metavar='File', type=argparse.FileType('w'), help='Output File', required=True)
-    parser.add_argument('-a', '--assembly', metavar='File', type=argparse.FileType('r'), help='Assembly Report File', default='data/GCF_000001405.25_GRCh37.p13_assembly_report.txt')
-    parser.add_argument('-s', '--style', metavar='str', choices=['ENSEMBL', 'NCBI'], help='GFF style', required=True)
-    parser.add_argument('--add_intron', help='Add intron to gff file', action='store_true')
+    parser = argparse.ArgumentParser(
+        description="Convert NCBI/ENSEMBL style Gff file to a concise UCSC style", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('-i', '--input',
+                        metavar='input_gff',
+                        type=argparse.FileType('r'),
+                        help='Input Gff File',
+                        required=True
+                        )
+
+    parser.add_argument('-o', '--output',
+                        metavar='output_gff',
+                        type=argparse.FileType('w'),
+                        help='Output Gff File',
+                        required=True
+                        )
+    parser.add_argument('-a', '--assembly', 
+                        metavar='assembly_report', 
+                        type=argparse.FileType('r'), 
+                        help='Assembly Report File', 
+                        required=True
+                        )
+    parser.add_argument('-t', '--table',
+                        metavar='output_name_id_table',
+                        type=argparse.FileType('w'),
+                        help='Output Name Id table File',
+                        required=True
+                        )
+    parser.add_argument('-s', '--style',
+                        metavar='gff_style',
+                        choices=['ENSEMBL', 'NCBI'],
+                        help='Gff style, "ENSEMBL" or "NCBI"',
+                        required=True
+                        )
+    parser.add_argument('--add_intron',
+                        help='Add intron to gff file',
+                        action='store_true'
+                        )
+    parser.add_argument('--add_utr',
+                        help='Add UTR to gff file',
+                        action='store_true'
+                        )
     args = parser.parse_args()
-    return Arags(gff_file=args.input, out_file=args.output, assembly_report_file=args.assembly, gff_style=args.style, add_intron=args.add_intron)
+    return Arags(gff_file=args.input, 
+                out_file=args.output, 
+                assembly_report_file=args.assembly, 
+                name_id_table=args.table,
+                gff_style=args.style, 
+                add_intron=args.add_intron, 
+                add_utr=args.add_utr
+                )
 
 
-def seqid_conversion(assembly_report_file, gff_style):
+def get_seqid_trans_table(assembly_report_file):
     """
     return seqid_conversion dict based on assembly report file
-    gff_style includs ESEMBLE AND NCBI
     """
     chroms = set([str(i) for i in range(1, 24)]) | set(['X', 'Y', 'MT'])
-    refseq_to_ucsc = dict()
-    genbank_to_ucsc = dict()
+    ncbi_to_ucsc = dict()
+    ensembl_to_ucsc = dict()
     for line in assembly_report_file:
         if line.startswith('#'):
             continue
         lst = line.strip().split('\t')
         seqid, refseq, genbank, ucsc = lst[0], lst[4], lst[6], lst[9]
         if seqid in chroms:
-            genbank_to_ucsc[seqid] = ucsc
+            ensembl_to_ucsc[seqid] = ucsc
         else:
-            genbank_to_ucsc[refseq] = ucsc
-        refseq_to_ucsc[genbank] = ucsc
-    if gff_style == 'ENSEMBL':
-        return genbank_to_ucsc
-    elif gff_style == 'NCBI':
-        return refseq_to_ucsc
+            ensembl_to_ucsc[refseq] = ucsc
+        ncbi_to_ucsc[genbank] = ucsc
+    return ncbi_to_ucsc, ensembl_to_ucsc
 
-args = get_args()
-seqid_trans_dict = seqid_conversion(args.assembly_report_file, args.gff_style)
-line_counts = 0
-seq_counts = 0
-gff_records = []
-pre_id = None
-pre_exon_record = None
 
-for line in args.gff_file:
-    if not line or line.startswith('#'):
-        continue
-    curr_record = GffRecord(line, args.gff_style)
-    if curr_record.id and (curr_record.type in SELECTED_TYPES):
-        pre_id = curr_record.id
-        curr_record = curr_record.convert(seqid_trans_dict)
-        if gff_records:
-            for _record in gff_records:
-                gff_records.sort(key = lambda x : int(x.start))
-                args.out_file.write(_record.string + '\n')
-                line_counts += 1
-        gff_records = []
-        if seq_counts != 0:
-            args.out_file.write('\n')
-        args.out_file.write(curr_record.string + '\n') # seperate line
-        seq_counts += 1
-        line_counts += 1
-    elif curr_record.parent == pre_id:
-        if curr_record.type in SELECTED_SUB_TYPES:
-            curr_record = curr_record.convert(seqid_trans_dict)
-            gff_records.append(curr_record)
+def get_trans_tables(seqid_trans_dict, type_trans_dict, atrb_trans_dict):
+    """
+    Get the tables for name transision
+    """
+    trans_tables = TransTables()
+    trans_tables.add_table('seqid', seqid_trans_dict)
+    trans_tables.add_table('type', type_trans_dict)
+    trans_tables.add_table('atrb', atrb_trans_dict)
+    return trans_tables
 
-        elif args.add_intron and curr_record.type == 'exon':
-            curr_record = curr_record.convert(seqid_trans_dict)
-            if pre_exon_record is not None and  pre_exon_record.type == 'exon' and curr_record.parent != None and curr_record.parent == pre_exon_record.parent:  # add intron record
-                _start = str(int(pre_exon_record.end) + 1)
-                _end = str(int(curr_record.start) - 1)
-                _atrb = f'Parent={curr_record.parent}'
-                if int(_end) >= int(_start):  # do not forget use int for comparision
-                    gff_records.append(GffRecord('\t'.join([curr_record.seqid, curr_record.source, 'intron', _start, _end, '.', curr_record.strand, '.', _atrb]), args.gff_style))
-            pre_exon_record = curr_record
+def read_gff(gff_file, gff_style, trans_tables):
+    """
+    1. For each gff record, if it belongs to selected types or subtypes, do the transformation and trimming
+    2. read selected_types into an OrderDict, selected_sub_types into a list
+    3. add selected_sub_type if its parent is in the OrderDict
+    4. return the OrderDict
+    """
+    id_gff_dict = OrderedDict([])  # gff_record.id : rna
+    subtype_gff_list = []
 
-if gff_records:
-    gff_records.sort(key = lambda x : int(x.start))
-    for _record in gff_records:
-        args.out_file.write(_record.string + '\n')
-        line_counts += 1
+    for line in gff_file:  # add selected type to id_list and id_gff_dict
+        if not line or line.startswith('#'):
+            continue
+        curr_record = GffRecord(line, gff_style)
+        if curr_record.type in SELECTED_TYPES:
+            curr_record = RNA(curr_record.convert(
+                trans_tables).string, gff_style)
+            if curr_record.id is not None:
+                id_gff_dict[curr_record.id] = curr_record
+        elif curr_record.type in SELECTED_SUB_TYPES and curr_record.parent is not None:
+            curr_record = curr_record.convert(trans_tables)
+            subtype_gff_list.append(curr_record)
 
-args.out_file.write('\n')
-args.assembly_report_file.close()
-args.gff_file.close()
-args.out_file.close()
-print (f"Written total {seq_counts} seqs and {line_counts} lines")
+    for gff in subtype_gff_list:  # add subtype as children if the parent is in id_list
+        if gff.parent in id_gff_dict:
+            id_gff_dict[gff.parent].add_children(gff)
+    
+    return id_gff_dict
+
+
+def write_table(gff_style, name_id_table, gff):
+    """
+    Write name id table file, the head is:
+    Name   NCBI   ENSEMBL
+    """
+    name = gff.name if gff.name else 'None'
+    source_id = gff.id if gff.id else 'None'
+    if gff_style == "NCBI":
+        name_id_table.write('\t'.join([name, source_id, 'None']) + '\n')
+    else:
+        name_id_table.write('\t'.join([name, 'None', source_id]) + '\n')
+
+def write_gff(out_file, add_intron, add_utr, gff_style, name_id_table, id_gff_dict):
+    """
+    write gff record to the output file
+    """
+    line_counts = 0
+    selected_type_counts = 0
+    for rna in id_gff_dict.values():
+        out_file.write(rna.string + ';\n')
+        write_table(gff_style, name_id_table, rna)
+        selected_type_counts += 1
+        sub_gff_list = []
+        if 'intron' not in rna.children and add_intron:
+            rna.add_intron()
+        if add_utr and '5_UTR' not in rna.children and '3_UTR' not in rna.children:
+            rna.add_utrs()
+        for sub_type in WRITE_SUB_TYPES:
+            sub_gff_list.extend(rna.get_children(sub_type))
+        sub_gff_list.sort(key=lambda x: int(x.start))
+        for _gff in sub_gff_list:
+            out_file.write(_gff.string + ';\n')
+            line_counts += 1
+        out_file.write('\n')
+    print(f"Written {selected_type_counts} seqs and {line_counts} lines")
+
+def main():
+    """
+    Main
+    """
+    args = get_args()
+    seqid_dicts = get_seqid_trans_table(args.assembly_report_file)
+    if args.gff_style == 'NCBI':
+        seqid_trans_dict = seqid_dicts[0]
+    elif args.gff_style == 'ENSEMBL':
+        seqid_trans_dict = seqid_dicts[1]
+    trans_tables = get_trans_tables(
+        seqid_trans_dict, TYPE_TRANS_DICT, ATRB_TRANS_DICT)
+    id_rna_dict = read_gff(args.gff_file, args.gff_style, trans_tables.tables)
+    args.name_id_table.write('\t'.join(['Name', 'NCBI', 'ENSEMBL']) + '\n') # header for name_id_table
+    write_gff(args.out_file, args.add_intron, args.add_utr,
+              args.gff_style, args.name_id_table, id_rna_dict)
+    args.assembly_report_file.close()
+    args.gff_file.close()
+    args.out_file.close()
+    args.name_id_table.close()
+
+if __name__ == "__main__":
+    main()
