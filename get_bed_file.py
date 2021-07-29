c7d2c6aef8201d60e3ffb20ae2f591e1728cfd78
donglf <here0009@gmail.com>
2021-07-29 13:52:54 +0800

    Use CHOOSEN_SUBTYPES to replace CDS

--- get_bed_file.py
+++ get_bed_file.py
@@ -17,7 +17,7 @@ optional arguments:
   -e extra_bp, --extra_bp extra_bp
 
 Example:
-    python3 get_bed_file.py -i get_coord/gene_list.bed -o get_coord/refGene.bed -g hg19_gff/UCSC_refGene_output.gff -e 10
+    python3 get_bed_file.py -i get_coord/gene_list.bed -o get_coord/refGene3.bed -g hg19_gff/UCSC_refGene_output.gff -e 10
     python3 get_bed_file.py -i get_coord/gene_list2.bed -o get_coord/ncbiRefSeq.bed -g hg19_gff/UCSC_ncbiRefSeq_output.gff -e 10
 
 Note:
@@ -27,9 +27,10 @@ Note:
 
 
 import argparse
-from typing import Counter, NamedTuple, OrderedDict, TextIO
+from typing import NamedTuple, OrderedDict, TextIO
 from GFF import RNA
-
+CHOOSEN_SUB_TYPES = {'CDS'}
+# CHOOSEN_SUB_TYPES = {'3_UTR', '5_UTR'}
 
 
 class Arags(NamedTuple):
@@ -87,6 +88,7 @@ def get_args():
                 extra_bp=args.extra_bp
                 )
 
+
 def trimm_id(ref_id):
     """
     trimm the version number after ncbi refseq id or ensembl id
@@ -129,7 +131,7 @@ def get_gff(query_id_dict, gff_fhand, gff_style):
     Get the gff file
     """
     id_gff_dict = dict() # id:gff_record
-    cds_list = []
+    children_list = []
     for line in gff_fhand:
         line = line.strip()
         if not line or line.startswith('#'):
@@ -137,9 +139,10 @@ def get_gff(query_id_dict, gff_fhand, gff_style):
         gff_record = RNA(line, gff_style)
         if gff_record.type == 'transcript' and gff_record.id in query_id_dict:
             id_gff_dict[gff_record.id] = gff_record
-        elif gff_record.type == 'CDS' and gff_record.parent in query_id_dict:
-            cds_list.append(gff_record)
-    for gff_cds in cds_list:
+        # elif gff_record.type == 'CDS' and gff_record.parent in query_id_dict:
+        elif gff_record.type in CHOOSEN_SUB_TYPES and gff_record.parent in query_id_dict:
+            children_list.append(gff_record)
+    for gff_cds in children_list:
         id_gff_dict[gff_cds.parent].add_children(gff_cds)
     return id_gff_dict
 
@@ -151,27 +154,30 @@ def get_bed(query_id_dict, id_gff_dict, output_fhand, extra_bp):
     bed_header = '\t'.join(['#chrom', 'start', 'end', 'strand', 'name', 'rna_id'])
     output_fhand.write(bed_header + '\n')
     not_found_names = []
-    no_cds_names = []
+    no_subtype_names = []
     for id in query_id_dict:
         if id not in id_gff_dict:
             not_found_names.append((id, query_id_dict[id]))
             continue
         gff_record = id_gff_dict[id]
-        cds_list = gff_record.get_children('CDS')
-        if len(cds_list) == 0:
-            no_cds_names.append((gff_record.id, gff_record.name))
-        for cds_gff_record in cds_list:
-            bed_record = get_bed_record(cds_gff_record, gff_record.name, extra_bp)
+        children_list = []
+        for _type in CHOOSEN_SUB_TYPES:
+            children_list.extend(gff_record.get_children(_type))
+        if len(children_list) == 0:
+            no_subtype_names.append((gff_record.id, gff_record.name))
+        children_list.sort(key=lambda x: int(x.start))
+        for child_record in children_list:
+            bed_record = get_bed_record(child_record, gff_record.name, extra_bp)
             output_fhand.write(bed_record + '\n')
 
     not_found_counts = len(not_found_names)
     total_queries = len(query_id_dict)
-    no_cds_counts = len(no_cds_names)
+    no_subtype_counts = len(no_subtype_names)
     print(f"There are {total_queries} total_queries")
     print(f'{not_found_counts} not found, the gene names not found are:')
     print(not_found_names)
-    print(f'{no_cds_counts} no CDS, the gene names without CDS are:')
-    print(no_cds_names)
+    print(f'{no_subtype_counts} no subtypes, the gene names without subtypes are:')
+    print(no_subtype_names)
 
 
 def main():
