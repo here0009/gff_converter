164db90fabed4f293c198f68897ec72811f1335d
donglf <here0009@gmail.com>
2021-08-04 18:00:00 +0800

    Add argparse

--- find_fusion_cosmic_table.py
+++ find_fusion_cosmic_table.py
@@ -1,15 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+usage: find_fusion_cosmic_table.py [-h] -i input_bed -f fusion_table -o output_dir
+
+Find the Gene Fusion Record in Cosmic Table of the given BED file, Group the Same Record Togther
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i input_bed, --input input_bed
+                        Input BED File (default: None)
+  -f fusion_table, --fusion_table fusion_table
+                        Fusion Table (default: None)
+  -o output_dir, --output_dir output_dir
+                        Output Directory (default: None)
+
+Example:
+    python3 find_fusion_cosmic_table.py -i get_coord/gene_list.bed -f data/CosmicFusionExport_GRCh37.tsv -o cosmic_fusion_report_grch37
+    python3 find_fusion_cosmic_table.py -i get_coord/red_gene_list.bed -f data/CosmicFusionExport_GRCh37.tsv -o cosmic_fusion_report_grch37/red_gene
+
+"""
+
 import pandas as pd
 import os.path
+import argparse
+from typing import NamedTuple, TextIO
+import os
 
-bed_file = 'get_coord/gene_list.bed'
-fusion_file = 'data/CosmicFusionExport_GRCh37.tsv'
-# output_file = 'get_coord/fusion_gene_cosmic.tsv'
-output_dir = 'cosmic_fusion_report_grch37'
-bed_fhand = open(bed_file)
-fusion_fhand = open(fusion_file)
-# output_fhand = open(output_file, 'w')
-PRIMARY_SITE = {'haematopoietic_and_lymphoid_tissue'}
-FUSION_TYPE = {'Observed mRNA'}
+class Arags(NamedTuple):
+    """
+    Command Line arguments
+    """
+    bed_file:TextIO
+    fusion_table:TextIO
+    output_dir:str
+
+
+
+def get_args():
+    """ 
+    Get command-line arguments
+    """
+    parser = argparse.ArgumentParser(
+        description="Find the Gene Fusion Record in Cosmic Table of the given BED file, Group the Same Record Togther", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('-i', '--input',
+                        metavar='input_bed',
+                        type=argparse.FileType('r'),
+                        help='Input BED File',
+                        required=True
+                        )
+
+    parser.add_argument('-f', '--fusion_table',
+                        metavar='fusion_table',
+                        type=argparse.FileType('r'),
+                        help='Fusion Table',
+                        required=True
+                        )
+    parser.add_argument('-o', '--output_dir',
+                        metavar='output_dir',
+                        help='Output Directory',
+                        required=True
+                        )
+    args = parser.parse_args()
+    return Arags(bed_file=args.input, 
+                fusion_table=args.fusion_table,
+                output_dir=args.output_dir
+                )
+
 
 def get_query_names(bed_fhand):
     """
@@ -23,7 +80,7 @@ def get_query_names(bed_fhand):
         query_names.add(line.split('\t')[4])
     return query_names
 
-def get_fusion_table(fusion_fhand):
+def get_fusion_table(fusion_fhand, PRIMARY_SITE, FUSION_TYPE):
     """
     Get the fusion table from the tsv file
     """
@@ -34,21 +91,36 @@ def get_fusion_table(fusion_fhand):
         fusion_table = fusion_table[fusion_table['FUSION_TYPE'].isin(FUSION_TYPE)]
     return fusion_table
 
-query_names = get_query_names(bed_fhand)
-print(query_names)
-fusion_table = get_fusion_table(fusion_fhand)
-print(fusion_table.head())
-end5_fusion_table = fusion_table[fusion_table["5'_GENE_NAME"].isin(query_names)]
-end3_fusion_table = fusion_table[fusion_table["3'_GENE_NAME"].isin(query_names)]
-one_end_fusion_table = fusion_table[fusion_table["3'_GENE_NAME"].isin(query_names) | fusion_table["5'_GENE_NAME"].isin(query_names)]
-two_ends_fusion_table = fusion_table[fusion_table["3'_GENE_NAME"].isin(query_names) & fusion_table["5'_GENE_NAME"].isin(query_names)]
-print(end5_fusion_table.head())
-print(end3_fusion_table.head())
-two_ends_fusion_table.to_csv(os.path.join(output_dir, 'two_ends.tsv'), sep = '\t', index = False)
-one_end_fusion_table.to_csv(os.path.join(output_dir, 'one_end.tsv'), sep = '\t', index = False)
-group_cols = ["FUSION_ID","TRANSLOCATION_NAME","5'_CHROMOSOME","5'_STRAND","5'_GENE_ID","5'_GENE_NAME","5'_LAST_OBSERVED_EXON","5'_GENOME_START_FROM","5'_GENOME_START_TO","5'_GENOME_STOP_FROM","5'_GENOME_STOP_TO","3'_CHROMOSOME","3'_STRAND","3'_GENE_ID","3'_GENE_NAME","3'_FIRST_OBSERVED_EXON","3'_GENOME_START_FROM","3'_GENOME_START_TO","3'_GENOME_STOP_FROM","3'_GENOME_STOP_TO"]
-# group_cols2 = ["FUSION_ID","TRANSLOCATION_NAME","5'_GENE_NAME","3'_GENE_NAME"]
-one_end_counts = one_end_fusion_table.groupby(by = group_cols).size().reset_index(name='counts')
-one_end_counts.to_csv(os.path.join(output_dir, 'one_end_count.tsv'), sep = '\t', index = False)
-two_end_counts = two_ends_fusion_table.groupby(by = group_cols).size().reset_index(name='counts')
-two_end_counts.to_csv(os.path.join(output_dir, 'two_ends_count.tsv'), sep = '\t', index = False)
+def get_fusion_report(fusion_table, query_names, output_dir):
+    """
+    Get the fusion record and the counts of fusion record in tsv file
+    """
+    os.makedirs(output_dir, exist_ok=True)
+    print(f"The output dir is {output_dir}")
+    one_end_fusion_table = fusion_table[fusion_table["3'_GENE_NAME"].isin(query_names) | fusion_table["5'_GENE_NAME"].isin(query_names)]
+    two_ends_fusion_table = fusion_table[fusion_table["3'_GENE_NAME"].isin(query_names) & fusion_table["5'_GENE_NAME"].isin(query_names)]
+    two_ends_fusion_table.to_csv(os.path.join(output_dir, 'two_ends.tsv'), sep = '\t', index = False)
+    one_end_fusion_table.to_csv(os.path.join(output_dir, 'one_end.tsv'), sep = '\t', index = False)
+    group_cols = ["FUSION_ID","TRANSLOCATION_NAME","5'_CHROMOSOME","5'_STRAND","5'_GENE_ID","5'_GENE_NAME","5'_LAST_OBSERVED_EXON","5'_GENOME_START_FROM","5'_GENOME_START_TO","5'_GENOME_STOP_FROM","5'_GENOME_STOP_TO","3'_CHROMOSOME","3'_STRAND","3'_GENE_ID","3'_GENE_NAME","3'_FIRST_OBSERVED_EXON","3'_GENOME_START_FROM","3'_GENOME_START_TO","3'_GENOME_STOP_FROM","3'_GENOME_STOP_TO"]
+    one_end_counts = one_end_fusion_table.groupby(by = group_cols).size().reset_index(name='counts')
+    one_end_counts.to_csv(os.path.join(output_dir, 'one_end_count.tsv'), sep = '\t', index = False)
+    print(f"There are {one_end_counts.shape[0]} one end fusions")
+    two_ends_counts = two_ends_fusion_table.groupby(by = group_cols).size().reset_index(name='counts')
+    two_ends_counts.to_csv(os.path.join(output_dir, 'two_ends_count.tsv'), sep = '\t', index = False)
+    print(f"There are {two_ends_counts.shape[0]} two ends fusions")
+
+def main():
+    PRIMARY_SITE = {'haematopoietic_and_lymphoid_tissue'}
+    FUSION_TYPE = {'Observed mRNA'}
+    args = get_args()
+    query_names = get_query_names(args.bed_file)
+    print("The names of the query genes are:")
+    print(query_names)
+    fusion_table = get_fusion_table(args.fusion_table, PRIMARY_SITE, FUSION_TYPE)
+    print("The head of the fusion table is:")
+    print(fusion_table.head())
+    get_fusion_report(fusion_table, query_names, args.output_dir)
+
+
+if __name__ == '__main__':
+    main()
